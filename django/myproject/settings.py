fb28cfee55bbfb17933345c9534c4cfe4b56d01c
Eclipse Client <alzex3@gmail.com>
2022-02-19 14:05:45 +0300

    Add environ

--- django/myproject/settings.py
+++ django/myproject/settings.py
@@ -1,4 +1,3 @@
-import os
 from environ import Env
 from pathlib import Path
 
@@ -78,14 +77,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_NAME'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': env('POSTGRES_HOST'),
-        'PORT': '5432',
-    }
+    'default': env.db()
 }
 
 # Password validation
